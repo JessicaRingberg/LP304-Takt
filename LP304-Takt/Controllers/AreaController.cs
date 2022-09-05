845d8ba58668e68ce8ca11eeec7c0b34c8dbce26
Jessica Ringberg <jessicaringberg@outlook.com>
2022-09-05 11:25:20 +0200

    added get area by id

--- LP304-Takt/Controllers/AreaController.cs
+++ LP304-Takt/Controllers/AreaController.cs
@@ -28,6 +28,23 @@ namespace LP304_Takt.Controllers
             return Ok();
         }
 
+        [HttpGet]
+        public async Task<ActionResult<List<AreaDto>>> GetAreas()
+        {
+            return Ok((await _areaService.GetEntities()).Select(c => c.AsDto()));
+        }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AreaDto>> GetArea(int id)
+        {
+            var area = await _areaService.GetEntity(id);
+
+            if (area is null)
+            {
+                return NotFound($"Area with {id} not found");
+            }
+
+            return Ok(area.AsDto());
+        }
     }
 }
