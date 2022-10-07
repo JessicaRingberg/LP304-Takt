44f321800c8dad6a2b3982b56f2e44e15864a90c
Jessica Ringberg <jessicaringberg@outlook.com>
2022-10-07 11:32:07 +0200

    Queue (#33)
    * u
    
    * article and orderDetails
    
    * orderDetails
    
    * order with order details
    
    * dtos
    
    * id
    
    * order with order details fixed
    
    * order details

--- LP304-Takt/Models/AlarmType.cs
+++ LP304-Takt/Models/AlarmType.cs
@@ -4,10 +4,6 @@ namespace LP304_Takt.Models
 {
     public class AlarmType
     {
-        //public AlarmType()
-        //{
-        //    Alarms = new List<Alarm>();
-        //}
         public int Id { get; set; }
         [Required]
         public string Name { get; set; } = string.Empty;
